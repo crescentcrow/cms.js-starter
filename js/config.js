fc1b3724b95d492e1a16d21b466d23bc22a22fdb
crescentcrow <siguredou@gmail.com>
2024-05-13 14:30:55 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var config = {
   github: {
     username: 'crescentcrow',
     repo: 'cms.js-starter',
-    branch: 'deverop',
+    branch: 'gh-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
     // prefix: 'subdirectory',
